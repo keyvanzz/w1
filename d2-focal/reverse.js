54e0aa0cb73a35cad951ed58b47a5e395d10497b
K1 <k.kholghi@gmail.com>
2019-09-19 01:45:19 +0000

    Added average.js and reverse.js

---
+++ d2-focal/reverse.js
@@ -0,0 +1,12 @@
+let input = process.argv[2];
+
+if (input) {
+  console.log(reverse(input));
+}
+
+const reverse = function(original) {
+  return original
+    .split("")
+    .reverse()
+    .join("-");
+};
