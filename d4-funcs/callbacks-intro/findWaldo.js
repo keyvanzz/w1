9c8c91e8a9435385576e77e5b378483529aa660d
K1 <k.kholghi@gmail.com>
2019-09-19 21:45:13 +0000

    Added findWaldo.js

--- d4-funcs/callbacks-intro/findWaldo.js
+++ d4-funcs/callbacks-intro/findWaldo.js
@@ -1,11 +1,9 @@
-// The second argument/parameter is expected to be a (callback) function
 const findWaldo = function(names, found) {
-  for (let i = 0; i < names.length; i++) {
-    let name = names[i];
+  names.forEach(function(name, index, aray) {
     if (name === "Waldo") {
-      found(i); // execute callback
+      found(index);
     }
-  }
+  });
 };
 
 const actionWhenFound = function(index) {
