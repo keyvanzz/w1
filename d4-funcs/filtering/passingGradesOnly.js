d03f93b061725d74861f5093e1d3a96f8fbf458a
K1 <k.kholghi@gmail.com>
2019-09-20 01:28:09 +0000

    Revised passingGradesOnly.js with arrow function

--- d4-funcs/filtering/passingGradesOnly.js
+++ d4-funcs/filtering/passingGradesOnly.js
@@ -1,7 +1,5 @@
 const grades = [73, 69, 3, 100, 50, 70, 69, 88, 95, 77, 35];
 
-const passedGrades = grades.filter(function(grade) {
-  return grade >= 70;
-});
+const passedGrades = grades.filter(grade => grade >= 70);
 
 console.log("The passed grades are:", passedGrades);
