3d2e716b41fc86c10d288bdd094b41f75c37d4cc
K1 <k.kholghi@gmail.com>
2019-09-22 09:11:03 +0000

    findKeyByValue.js

---
+++ lotide/findKeyByValue.js
@@ -0,0 +1,31 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(
+      `\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`
+    );
+  } else if (actual !== expected) {
+    console.log(
+      `\uD83D\uDD34\uD83D\uDD34\uD83D\uDD34 Assertion Failed: ${actual} !== ${expected}`
+    );
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+const findKeyByValue = function(object, value) {
+  const keys = Object.keys(object);
+  for (let key of keys) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+// TEST CODE
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
