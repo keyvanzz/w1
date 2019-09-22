0d07b0e0ad3616bf1a433fb2e0e0bd9ebabf7895
K1 <k.kholghi@gmail.com>
2019-09-22 04:09:10 +0000

    Revised the emojies Hexas in all files for assertions passed and failed

--- lotide/assertArraysEqual.js
+++ lotide/assertArraysEqual.js
@@ -1,15 +1,19 @@
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    console.log(`Assertion Failed: ${array1} === ${array2}`);
+    console.log(
+      `\uD83D\uDD34\uD83D\uDD34\uD83D\uDD34 Assertion Failed: ${array1} === ${array2}`
+    );
     return;
   }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      console.log(`Assertion Failed: ${array1} === ${array2}`);
+      console.log(
+        `\uD83D\uDD34\uD83D\uDD34\uD83D\uDD34 Assertion Failed: ${array1} === ${array2}`
+      );
       return;
     }
   }
-  console.log(`Assertion Passed: ${array1} === ${array2}`);
+  console.log(`\u2705\u2705\u2705 Assertion Passed: ${array1} === ${array2}`);
 };
 // TEST CODE
 eqArrays([1, 2, 3], [1, 2, 3]); // => should
--- lotide/countOnly.js
+++ lotide/countOnly.js
@@ -1,9 +1,13 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(
+      `\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`
+    );
   } else if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(
+      `\uD83D\uDD34\uD83D\uDD34\uD83D\uDD34 Assertion Failed: ${actual} !== ${expected}`
+    );
   }
 };
 
