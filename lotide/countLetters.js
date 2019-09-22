13ab763bb8263db8daf6ee4c0013352dc05ad6ee
K1 <k.kholghi@gmail.com>
2019-09-22 08:23:01 +0000

    Revised countLetters.js

--- lotide/countLetters.js
+++ lotide/countLetters.js
@@ -11,6 +11,23 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+let sentence = "lighthouse in the house";
+sentence = sentence.replace(/\s+/g, ""); //Removes spaces from string
+const countLetters = function(text) {
+  const result = {};
+  const letters = sentence.split("");
+  for (let letter of letters) {
+    letter = letter.toLowerCase(); //Converts to lower case
+    if (result[letter]) {
+      //If result is in the object
+      result[letter] += result[letter]; //It will be added to total
+    } else {
+      //If result is not in the object
+      result[letter] = 1; //It will add additional key
+    }
+  }
+  return result;
+};
+
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+console.log(countLetters(sentence));
