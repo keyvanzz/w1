3cb27346da9c45dd5c5e38ecc948099b4a54dd72
K1 <k.kholghi@gmail.com>
2019-09-23 21:33:16 +0000

    nothing

--- lotide/test/assertEqualTest.js
+++ lotide/test/assertEqualTest.js
@@ -3,11 +3,3 @@ const assertEqual = require(`../assertEqual`);
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
-
-assertArraysEqual.js;
-assertEqual.js;
-eqArrays.js;
-head.js;
-middle.js;
-tail.js;
-test/
