7170fdc3570ccde8e5a340213d87b7f7788c5277
K1 <k.kholghi@gmail.com>
2019-09-23 22:50:09 +0000

    Convert test file to accept mocha and chai

--- lotide/test/headTest.js
+++ lotide/test/headTest.js
@@ -1,8 +1,28 @@
-const assertEqual = require(`../assertEqual`);
+const assert = require(`chai`).assert;
 const head = require(`../head`);
 
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Yo Yo", "Lighthouse", "Labs"]), "Yo Yo");
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+
+  it("returns '5' for ['5, 6, 7']", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("returns 'Yo Yo' for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Yo Yo", "Lighthouse", "Labs"]), "Yo Yo");
+  });
+
+  it("returns '1' for ['1']", () => {
+    assert.strictEqual(head([1]), 1);
+  });
+
+  it("returns 'undefined' for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
