f97cfded5675a30fb074d2960ec2e702c960dba7
K1 <k.kholghi@gmail.com>
2019-09-24 03:34:20 +0000

    Added assertions to findKey.js

--- lotide/findKey.js
+++ lotide/findKey.js
@@ -1,4 +1,16 @@
-const findKey = function(obj, callback) {
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(
+      `\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`
+    );
+  } else if (actual !== expected) {
+    console.log(
+      `\uD83D\uDD34\uD83D\uDD34\uD83D\uDD34 Assertion Failed: ${actual} !== ${expected}`
+    );
+  }
+};
+
+const findKey = function (obj, callback) {
   const keys = Object.keys(obj);
 
   for (let key of keys) {
@@ -9,4 +21,13 @@ const findKey = function(obj, callback) {
   return undefined;
 };
 
+//Tests
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
 module.exports = findKey;
